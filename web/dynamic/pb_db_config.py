351769130086eb037d79fac8bf166f94ee874ec2
Euku <euku@arcor.de>
2015-10-17 09:11:21 +0000

    new paths

--- web/dynamic/pb_db_config.py
+++ web/dynamic/pb_db_config.py
@@ -11,9 +11,9 @@ index_path = "https://tools.wmflabs.org/pb/index.py?p=%(page)s&%(args)s"
 # path to the www static directory
 static_path = "https://tools.wmflabs.org/pb/static/"
 # path to the local base directory
-base_path = os.path.expanduser("~local-pb/pb/web/")
+base_path = os.path.expanduser("~tools.pb/pb/web/")
 # path to the logging directory
-log_path = os.path.expanduser("~local-pb/log")
+log_path = os.path.expanduser("~tools.pb/log/pb")
 # path to the database conf file
 db_conf_file = os.path.expanduser("~/replica.pb-db.cnf")
 # database
